658aa564c8963b2e6659f83c30488fd923fa8b4c
issui1980 <60308425+issui1980@users.noreply.github.com>
2020-03-14 21:41:43 +0900

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text;
+using System.Threading;
 
 namespace TestConsole
 {
@@ -6,7 +8,15 @@ namespace TestConsole
     {
         static void Main(string[] args)
         {
+
+            StringBuilder execInfo = new StringBuilder();
+            execInfo.AppendLine(Environment.UserDomainName);
+            //execInfo.AppendLine(Thread.CurrentPrincipal.Identity.Name);
+            execInfo.AppendLine(System.Environment.UserName);
+            execInfo.AppendLine(AppDomain.CurrentDomain.BaseDirectory);
+
             Console.WriteLine("Hello World!");
+            Console.WriteLine(execInfo.ToString());
         }
     }
 }
